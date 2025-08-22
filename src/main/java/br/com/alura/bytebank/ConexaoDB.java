2b9792ba3729b82c01bf7289cf1523c30f6da893
alex-isidro <alexander.d.isidro@gmail.com>
2025-08-21 21:36:15 -0300

    conexão com um banco de dados

---
+++ src/main/java/br/com/alura/bytebank/ConexaoDB.java
@@ -0,0 +1,21 @@
+package br.com.alura.bytebank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoDB {
+    public static void main(String... x) {
+
+        String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
+        String usuario = "rm565554";
+        String senha = "230806";
+
+        try {
+            Connection con = DriverManager.getConnection(url, usuario, senha);
+            System.out.println("Conexão feita com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
